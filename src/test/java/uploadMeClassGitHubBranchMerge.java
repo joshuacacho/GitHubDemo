55c65a8646102c81896ceb638f25095a139e074b
Cacho <joshcacho2@hotmail.com>
2022-04-18 12:07:34 -0700

    Added new class

---
+++ src/test/java/uploadMeClassGitHubBranchMerge.java
@@ -0,0 +1,11 @@
+
+public class uploadMeClassGitHubBranchMerge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("I was merged into master on " + System.currentTimeMillis());
+
+	}
+
+}
